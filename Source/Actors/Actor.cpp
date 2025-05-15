1ec3fc26aad3cb19d3b8c8d062cdb5b29e816424
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-05-15 02:32:44 -0300

    feat: adding actors base class

---
+++ Source/Actors/Actor.cpp
@@ -0,0 +1,58 @@
+#include "Actor.h"
+#include "../Game.h"
+#include "../Components/Component.h"
+#include <algorithm>
+
+Actor::Actor(Game *game) {
+    mState = ActorState::Active;
+    mPosition = Vector2::Zero;
+    mScale = 1.0f;
+    mRotation = 0.0f;
+    mGame = game;
+    mIsOnGround = false;
+    mGame->AddActor(this);
+}
+
+Actor::~Actor() {
+    mGame->RemoveActor(this);
+
+    for (const auto component : mComponents)
+        delete component;
+
+    mComponents.clear();
+}
+
+void Actor::Update(const float deltaTime) {
+    if (mState == ActorState::Active) {
+        for (auto comp : mComponents) {
+            if(comp->IsEnabled())
+                comp->Update(deltaTime);
+        }
+
+        OnUpdate(deltaTime);
+    }
+}
+
+void Actor::OnUpdate(float deltaTime) {}
+
+void Actor::OnCollision(const float minOverlap, AABBColliderComponent *other) {}
+
+void Actor::Kill() {}
+
+void Actor::ProcessInput(const Uint8 *keyState) {
+    if (mState == ActorState::Active) {
+        for (const auto comp : mComponents)
+            comp->ProcessInput(keyState);
+
+        OnProcessInput(keyState);
+    }
+}
+
+void Actor::OnProcessInput(const Uint8 *keyState) {}
+
+void Actor::AddComponent(Component *c) {
+    mComponents.emplace_back(c);
+    std::sort(mComponents.begin(), mComponents.end(), [](Component* a, Component* b) {
+        return a->GetUpdateOrder() < b->GetUpdateOrder();
+    });
+}
