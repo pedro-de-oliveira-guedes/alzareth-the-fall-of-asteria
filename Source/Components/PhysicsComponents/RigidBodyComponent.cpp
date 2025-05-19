83d6d2d39a55afe318ee61300f8068c7caa97091
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-05-19 19:53:34 -0300

    feat: applying physics in the y-axis

--- Source/Components/PhysicsComponents/RigidBodyComponent.cpp
+++ Source/Components/PhysicsComponents/RigidBodyComponent.cpp
@@ -27,6 +27,9 @@ void RigidBodyComponent::Update(float deltaTime) {
     if(Math::Abs(mVelocity.x) > 0.05f) {
         ApplyForce(Vector2::UnitX * -mFrictionCoefficient * mVelocity.x);
     }
+    if (Math::Abs(mVelocity.y) > 0.05f) {
+        ApplyForce(Vector2::UnitY * -mFrictionCoefficient * mVelocity.y);
+    }
 
     // Euler Integration
     mVelocity += mAcceleration * deltaTime;
@@ -34,8 +37,11 @@ void RigidBodyComponent::Update(float deltaTime) {
     mVelocity.x = Math::Clamp<float>(mVelocity.x, -MAX_SPEED_X, MAX_SPEED_X);
     mVelocity.y = Math::Clamp<float>(mVelocity.y, -MAX_SPEED_Y, MAX_SPEED_Y);
 
-    if(Math::NearZero(mVelocity.x, 1.0f)) {
+    if (Math::NearZero(mVelocity.x, 1.0f)) {
         mVelocity.x = 0.f;
+    }
+    if (Math::NearZero(mVelocity.y, 1.0f)) {
+        mVelocity.y = 0.f;
     }
 
     const auto collider = mOwner->GetComponent<AABBColliderComponent>();
