2a2341ab7d44d08f345547b1b2424aeaabd1b7fd
Gabrielzzz7 <116455055+Gabrielzzz7@users.noreply.github.com>
2025-06-20 15:48:41 -0300

    feat: hud of inventory and items in the inventory

--- Source/Components/UI/HUDComponent.cpp
+++ Source/Components/UI/HUDComponent.cpp
@@ -3,9 +3,10 @@
 #include "../../Game.h"
 #include "../../Actors/Actor.h"
 #include "../../Utils/Math.h"
+#include "../../UIElements/UIImage.h" 
 
 HUDComponent::HUDComponent(
-    Actor *owner,
+    Actor* owner,
     const float maxHealth,
     const float maxEnergy,
     const int drawOrder
@@ -15,12 +16,19 @@ HUDComponent::HUDComponent(
     mMaxEnergy = maxEnergy;
     mCurrentEnergy = maxEnergy;
 
-    mHealthColor = {255, 0, 0, 255}; // Red
-    mEnergyColor = {255, 255, 0, 255}; // Yellow
-    mBarBkgColor = {255, 255, 255, 255}; // Black
+    mHealthColor = { 255, 0, 0, 255 }; // Red
+    mEnergyColor = { 255, 255, 0, 255 }; // Yellow
+    mBarBkgColor = { 255, 255, 255, 255 }; // Black
 
-    mHealthBarBkg = {0, 0, 0, 0};
-    mEnergyBarBkg = {0, 0, 0, 0};
+    mHealthBarBkg = { 0, 0, 0, 0 };
+    mEnergyBarBkg = { 0, 0, 0, 0 };
+
+    int windowHeight = mOwner->GetGame()->GetWindowHeight();
+
+    mInventoryImage = new UIImage(
+        "../Assets/Inventory/Inventory.png",
+        Vector2(100.0f, static_cast<float>(windowHeight) - 144.0f - 40.0f),
+        Vector2(300.0f, 244.0f));
 }
 
 void HUDComponent::UpdateStats(
@@ -35,7 +43,11 @@ void HUDComponent::UpdateStats(
     mCurrentEnergy = currentEnergy;
 }
 
-void HUDComponent::DrawHealthBar(SDL_Renderer *renderer) {
+void HUDComponent::Update(float deltaTime) {
+    UpdateInventoryDisplay();
+}
+
+void HUDComponent::DrawHealthBar(SDL_Renderer* renderer) {
     const int screenWidth = mOwner->GetGame()->GetWindowWidth();
     const int screenHeight = mOwner->GetGame()->GetWindowHeight();
 
@@ -58,7 +70,7 @@ void HUDComponent::DrawHealthBar(SDL_Renderer *renderer) {
     SDL_RenderFillRect(renderer, &healthBarRect);
 }
 
-void HUDComponent::DrawEnergyBar(SDL_Renderer *renderer) {
+void HUDComponent::DrawEnergyBar(SDL_Renderer* renderer) {
     const int healthBarBottom = mHealthBarBkg.y + mHealthBarBkg.h;
     const int screenHeight = mOwner->GetGame()->GetWindowHeight();
 
@@ -81,7 +93,56 @@ void HUDComponent::DrawEnergyBar(SDL_Renderer *renderer) {
     SDL_RenderFillRect(renderer, &energyBarRect);
 }
 
-void HUDComponent::Draw(SDL_Renderer *renderer) {
+void HUDComponent::DrawInventoryImage(SDL_Renderer* renderer) {
+    mInventoryImage->Draw(renderer, Vector2::Zero);
+}
+
+void HUDComponent::DrawInventory(SDL_Renderer* renderer) {
+    for (const auto& image : mInventoryItems) {
+        if (image) {
+            image->Draw(renderer, Vector2::Zero);
+        }
+    }
+}
+
+void HUDComponent::Draw(SDL_Renderer* renderer) {
     DrawHealthBar(renderer);
     DrawEnergyBar(renderer);
+    DrawInventoryImage(renderer);
+    DrawInventory(renderer);
+}
+
+void HUDComponent::UpdateInventoryDisplay() {
+    for (auto image : mInventoryItems) {
+        delete image;
+    }
+    mInventoryItems.clear();
+    
+    const Inventory& playerInventory = static_cast<Player*>(mOwner->GetGame()->GetPlayer())->GetInventory();
+    
+    float itemDisplayY = mInventoryImage->GetPosition().y + 95.0f; 
+
+    // Espaçamento e tamanho dos itens
+    float initialPaddingX = 18.0f; 
+    float itemSpacingX = 10.5f; 
+    float itemImageSize = 44.0f; 
+
+    
+    float currentX = mInventoryImage->GetPosition().x + initialPaddingX;
+   
+    int item_count = 0;
+    for (const auto& item : playerInventory.GetItems()) { 
+
+        Vector2 itemPos = Vector2(currentX, itemDisplayY);
+        Vector2 itemSize = Vector2(itemImageSize, itemImageSize);
+
+        UIImage* itemImage = new UIImage(item->GetTexturePath(),
+            itemPos,
+            itemSize);
+        mInventoryItems.push_back(itemImage);
+
+        currentX += itemImageSize + itemSpacingX; 
+        
+        item_count++;
+    }
 }
