c9a20c095df16accbaf96d8d25333a2798f00ace
Gabrielzzz7 <116455055+Gabrielzzz7@users.noreply.github.com>
2025-07-06 13:13:30 -0300

    feat:: random drop items

--- Source/Actors/Skeleton/Skeleton.cpp
+++ Source/Actors/Skeleton/Skeleton.cpp
@@ -13,7 +13,7 @@ const std::string WALKING_ANIMATION = "walking";
 const std::string ATTACK_ANIMATION = "attack";
 
 
-Skeleton::Skeleton(Game *game, Vector2 position) : Enemy(game) {
+Skeleton::Skeleton(Game* game, Vector2 position) : Enemy(game) {
 
     SetPosition(position);
 
@@ -21,21 +21,21 @@ Skeleton::Skeleton(Game *game, Vector2 position) : Enemy(game) {
     mCurrentHealth = mMaxHealth;
     mIsDead = false;
 
-    mWalkSpeed =  50.0f;
+    mWalkSpeed = 50.0f;
 
     mRigidBodyComponent = new RigidBodyComponent(this, 1.0f, 5.0f);
     int SkeletonSize = 99;
     int colliderSize = 80;
-    
+
     int offsetX = (SkeletonSize - colliderSize) / 2;
     int offsetY = (SkeletonSize - colliderSize) / 2;
-    
+
     mColliderComponent = new AABBColliderComponent(
-        this, 
-        offsetX,           
-        offsetY,           
-        colliderSize,      
-        colliderSize,      
+        this,
+        offsetX,
+        offsetY,
+        colliderSize,
+        colliderSize,
         ColliderLayer::Enemy,
         false,
         true
@@ -52,9 +52,9 @@ Skeleton::Skeleton(Game *game, Vector2 position) : Enemy(game) {
         100
     );
 
-    mDrawComponent->AddAnimation(IDLE_ANIMATION, {0});
-    mDrawComponent->AddAnimation(ATTACK_ANIMATION, {1, 2});
-    mDrawComponent->AddAnimation(WALKING_ANIMATION, {3, 5, 7});
+    mDrawComponent->AddAnimation(IDLE_ANIMATION, { 0 });
+    mDrawComponent->AddAnimation(ATTACK_ANIMATION, { 1, 2 });
+    mDrawComponent->AddAnimation(WALKING_ANIMATION, { 3, 5, 7 });
     mDrawComponent->SetAnimFPS(10.f);
 
     mDrawComponent->SetAnimation(IDLE_ANIMATION);
@@ -65,7 +65,7 @@ void Skeleton::Attack() {
         return;
     }
 
-    auto *player = mGame->GetPlayer();
+    auto* player = mGame->GetPlayer();
 
     if (!player || player->GetIsDashing()) {
         return;
@@ -81,7 +81,7 @@ void Skeleton::Attack() {
     Vector2 direction = playerPos - GetPosition();
     direction.Normalize();
 
-    new Fireball(mGame, GetPosition(), direction * 200.0f, mDamageAttack); 
+    new Fireball(mGame, GetPosition(), direction * 200.0f, mDamageAttack);
     ManageAnimations();
 
     mAttackCooldown = 3.0f; // reset
@@ -95,30 +95,32 @@ void Skeleton::OnUpdate(float deltaTime) {
     if (mAttackCooldown > 0.0f) {
         mAttackCooldown -= deltaTime;
     }
-    
-    Vector2 EnemyPos  = GetPosition();
+
+    Vector2 EnemyPos = GetPosition();
 
     // add small random offset to the enemy position
     EnemyPos.x += (std::rand() % 10 - 5) * 0.1f; // random offset between -0.5 and 0.5
     EnemyPos.y += (std::rand() % 10 - 5) * 0.1f; // random offset between -0.5 and 0.5
 
     Vector2 playerPos = mGame->GetPlayer()->GetPosition();
-    Vector2 toPlayer  = playerPos - EnemyPos;
+    Vector2 toPlayer = playerPos - EnemyPos;
 
     // rotate based on player position
     if (toPlayer.x < 0.0f) {
         SetRotation(Math::Pi);
-    } else {
+    }
+    else {
         SetRotation(0.0f);
     }
 
     float distance = toPlayer.Length();
 
-    if (distance > 0.0f) toPlayer *= 1/distance; // normalize
+    if (distance > 0.0f) toPlayer *= 1 / distance; // normalize
 
     if (distance < 450.0f) {
         Attack();
-    } else {
+    }
+    else {
         mIsWalking = true;
         mIsAttacking = false;
 
@@ -144,24 +146,38 @@ void Skeleton::Kill() {
     // get random int
     int randomInt = std::rand() % 100;
 
-    if (randomInt < 20) {
+    if (randomInt < 40) {
         new CollectibleItem(mGame, "Energy_Potion", Item::ItemType::Consumable,
             "../Assets/Sprites/Items/Energy/energy_potion.png",
             "../Assets/Sprites/Items/Energy/energy_potion_inventory.png",
             "../Assets/Sprites/Items/Energy/energy_potion.json",
             1, GetPosition());
-    } else if (randomInt < 50) {
+    }
+    else if (randomInt < 70) {
         new CollectibleItem(mGame, "Health_Potion", Item::ItemType::Consumable,
             "../Assets/Sprites/Items/Health/health_potion.png",
             "../Assets/Sprites/Items/Health/health_potion_inventory.png",
             "../Assets/Sprites/Items/Health/health_potion.json",
             1, GetPosition());
-    } else if(randomInt < 100) {
+    }
+    else if (randomInt < 90) {
         new CollectibleItem(mGame, "Invulnerability_Potion", Item::ItemType::Consumable,
             "../Assets/Sprites/Items/Invulnerability/invulnerability_potion.png",
             "../Assets/Sprites/Items/Invulnerability/invulnerability_potion_inventory.png",
             "../Assets/Sprites/Items/Invulnerability/invulnerability_potion.json",
             1, GetPosition());
+    }
+    else {
+        if (mGame->GetMagicTokenInWorld()) {
+            return; // don't drop if already in world
+        }
+        else {
+            new CollectibleItem(mGame, "Magic_Token", Item::ItemType::RangedWeapon,
+                "../Assets/Sprites/Items/MagicToken/magic_token.png",
+                "../Assets/Sprites/Items/MagicToken/magic_token_inventory.png",
+                "../Assets/Sprites/Items/MagicToken/magic_token.json",
+                1, GetPosition());
+        }
     }
     // TODO: Add more items to drop
 }
@@ -169,20 +185,22 @@ void Skeleton::Kill() {
 void Skeleton::ManageAnimations() const {
     if (mIsAttacking) {
         mDrawComponent->SetAnimation(ATTACK_ANIMATION);
-    } else if (!mIsWalking) {
+    }
+    else if (!mIsWalking) {
         mDrawComponent->SetAnimation(IDLE_ANIMATION);
-    } else if (mIsWalking) {
+    }
+    else if (mIsWalking) {
         mDrawComponent->SetAnimation(WALKING_ANIMATION);
     }
 }
 
-void Skeleton::OnCollision(float minOverlap, AABBColliderComponent *other) {
+void Skeleton::OnCollision(float minOverlap, AABBColliderComponent* other) {
 
-    if(other->GetLayer() == ColliderLayer::PlayerProjectile){
+    if (other->GetLayer() == ColliderLayer::PlayerProjectile) {
         auto projectile = dynamic_cast<Projectile*>(other->GetOwner());
         if (projectile) {
-            TakeDamage(projectile->GetDamage()); 
-            projectile->SetState(ActorState::Destroy); 
+            TakeDamage(projectile->GetDamage());
+            projectile->SetState(ActorState::Destroy);
             mGame->RemoveActor(projectile);
         }
     }
