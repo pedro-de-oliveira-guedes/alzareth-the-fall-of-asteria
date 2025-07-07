2c9d94c844ba0d501b193c360ae98584ac96b998
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-07-07 00:59:16 -0300

    fix: using different sprites for vertical and horizontal rays

--- Source/Actors/Alzareth/Attacks/DeathRay.cpp
+++ Source/Actors/Alzareth/Attacks/DeathRay.cpp
@@ -11,17 +11,18 @@ DeathRay::DeathRay(
 ) : Actor(game) {
     mEndPosition = end_position;
     mTimeUntilActive = timeUntilActive;
+    std::vector<int> sprites;
 
     const float xOffset = std::abs(end_position.x - start_position.x);
     const float yOffset = std::abs(end_position.y - start_position.y);
     if (xOffset > yOffset) {
         mVelocity = Vector2(1.0f, 0.f) * mSpeed;
         mSize = Vector2(BASE_WIDTH, mGame->GetSceneManager()->GetLevelSize().second * SceneManagerSystem::TILE_SIZE);
-        mRotation = 0.0f;
+        sprites = { 2, 3 };
     } else {
         mVelocity = Vector2(0.f, 1.f) * mSpeed;
         mSize = Vector2(mGame->GetSceneManager()->GetLevelSize().first * SceneManagerSystem::TILE_SIZE, BASE_WIDTH);
-        mRotation = 90.0f;
+        sprites = { 0, 1 };
     }
 
     mRigidBodyComponent = new RigidBodyComponent(this);
@@ -45,7 +46,7 @@ DeathRay::DeathRay(
         "../Assets/Sprites/Alzareth/DeathRay/Alzareth_Death_Ray.json",
         mRotation
     );
-    mDrawAnimatedComponent->AddAnimation("default", { 0, 1 });
+    mDrawAnimatedComponent->AddAnimation("default", sprites);
     mDrawAnimatedComponent->SetAnimation("default");
     mDrawAnimatedComponent->SetAnimFPS(10.f);
     mDrawAnimatedComponent->SetEnabled(false);
@@ -53,19 +54,12 @@ DeathRay::DeathRay(
     SetPosition(start_position);
 }
 
-DeathRay::~DeathRay() {
-    delete mDrawAnimatedComponent;
-    delete mColliderComponent;
-}
+DeathRay::~DeathRay() {}
 
 void DeathRay::OnUpdate(const float deltaTime) {
     if (mTimeUntilActive > 0.0f) {
         mTimeUntilActive -= deltaTime;
         return;
-    } else {
-        mRigidBodyComponent->SetEnabled(true);
-        mColliderComponent->SetEnabled(true);
-        mDrawAnimatedComponent->SetEnabled(true);
     }
 
     if (mIsFinished) {
@@ -75,9 +69,13 @@ void DeathRay::OnUpdate(const float deltaTime) {
         return;
     }
 
+    mRigidBodyComponent->SetEnabled(true);
+    mColliderComponent->SetEnabled(true);
+    mDrawAnimatedComponent->SetEnabled(true);
+
     mRigidBodyComponent->SetVelocity(mVelocity);
     mDamageCooldown -= deltaTime;
-    mDamageCooldown  = std::max(mDamageCooldown, 0.0f);
+    mDamageCooldown = std::max(mDamageCooldown, 0.0f);
 
     if (
         std::abs(GetPosition().x - mEndPosition.x) <= 2*(deltaTime * mSpeed) &&
